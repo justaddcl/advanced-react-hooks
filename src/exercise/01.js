81845e7238eebc5f5b7aee4dbc7e454c9664e345
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-10-23 15:40:38 -0500

    adds solution for ec4

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -4,16 +4,19 @@
 import React from 'react';
 
 function Counter({initialCount = 0, step = 1}) {
-  const countReducer = (state, action) => ({
-    ...state,
-    ...(typeof action === 'function' ? action(state) : state),
-  });
-  const [state, setState] = React.useReducer(countReducer, {
+  const countReducer = (state, action) => {
+    switch (action.type) {
+      case 'INCREMENT':
+        return {count: state.count + action.step};
+      default:
+        throw new Error(`Invalid action type: ${action.type}`);
+    }
+  };
+  const [state, dispatch] = React.useReducer(countReducer, {
     count: initialCount,
   });
   const {count} = state;
-  const increment = () =>
-    setState(currentState => ({count: currentState.count + step}));
+  const increment = () => dispatch({type: 'INCREMENT', step});
   return (
     <button type="button" onClick={increment}>
       {count}
