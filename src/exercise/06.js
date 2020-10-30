9283f86a327a3c0eb0daf447864717f4bae29453
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-10-30 16:10:35 -0500

    adds solution for ec1

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -3,9 +3,11 @@
 
 import React from 'react';
 
+const formatQueryState = ({query, state}) => `\`${query}\` => ${state}`;
+
 function useMedia(query, initialState = false) {
   const [state, setState] = React.useState(initialState);
-  React.useDebugValue(`\`${query}\` => ${state}`);
+  React.useDebugValue({query, state}, formatQueryState);
 
   React.useEffect(() => {
     let mounted = true;
